2be7e511f88ba4ae568ddc4ccb2121e0b2521da1
Joyrudra Biswas <joyrudra@itobuz.com>
2023-11-24 12:39:07 +0530

    ADded a Middleware to check userPatform

---
+++ src/middlewares/auth/check.request.platform.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { UserTypes } from "../../enums/types.enum";
+import StatusCodes from "../../enums/statusCodes.enum";
+
+export default async function checkRequestPlatform(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const headerType = req.headers.app;
+
+    if (!headerType) {
+      res.status(StatusCodes.FORBIDDEN);
+      throw new Error("Denied");
+    }
+
+    let reqPlatform = "";
+
+    if (headerType === UserTypes.ADMIN) {
+      reqPlatform = UserTypes.ADMIN;
+    }
+
+    reqPlatform = UserTypes.APP;
+
+    req.body.req.body.authData.reqPlatform = reqPlatform;
+  } catch (error) {
+    next(error);
+  }
+}
